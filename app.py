178c7a811902130cb1b75f6030c59d8533efe2f7
fabbrob <robfabbro50@gmail.com>
2022-07-22 16:43:50 +1000

    updated app.py to connect to db

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import os
+import psycopg2
+import bcrypt
 
 app = Flask(__name__)
 
+# If the DATABASE_URL is set, use that, otherwuse use local db.
+DB_URL = os.environ.get("DATABASE_URL", "dbname=esport_tipping") # Put your DB name
+
 @app.route('/')
 def index():
-    return '<h1>Hello, Robert!</h1>'
+  conn = psycopg2.connect(DB_URL)
+  cur = conn.cursor()
+  cur.execute('SELECT 1', []) # Query to check that the DB connected
+  conn.close()
+  return 'Hello, world!'
 
 if __name__ == "__main__":
     app.run(debug=True)
